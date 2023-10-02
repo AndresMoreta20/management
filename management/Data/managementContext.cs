250a403ed7ab3d77ee0c06aec65a2ecd72216284
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-10-02 15:23:53 -0500

    Add project files.

---
+++ management/Data/managementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using management.Models;
+
+namespace management.Data
+{
+    public class managementContext : DbContext
+    {
+        public managementContext (DbContextOptions<managementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<management.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ management/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace management.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        /*
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee { FirstName = "Juan", LastName = "Lopez" });
+            employees.Add(new Employee { FirstName = "Maria", LastName = "Hernandez" });
+            return employees;
+        }*/
+    }
+}
