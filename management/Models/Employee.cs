c6e38cdfd6e56fbca719bc1a5a9b573687ba3ade
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-10-02 19:43:53 -0500

    Edit comments

--- management/Models/Employee.cs
+++ management/Models/Employee.cs
@@ -5,13 +5,6 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        /*
-        public List<Employee> GetEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-            employees.Add(new Employee { FirstName = "Juan", LastName = "Lopez" });
-            employees.Add(new Employee { FirstName = "Maria", LastName = "Hernandez" });
-            return employees;
-        }*/
+  
     }
 }
